96eca19ea83b98970af07e5b63c058efa4664cff
asos-permyghataorre <permjit.ghataorre@asos.com>
2026-04-24 14:06:24 +0100

    fix: lint errors

--- src/components/ShareMedia/ShareMedia.tsx
+++ src/components/ShareMedia/ShareMedia.tsx
@@ -18,34 +18,29 @@ const ShareMedia = ({ mixItem }: TMixTapeItem) => {
 		? encodeURI(`I want to share this music mix with you from ---  ${config.baseUrl}#${mixItem?.mixId}`)
 		: encodeURI(`I want to share A Live Stream mix from --- ${liveSessionUrl}`);
 
-
-	console.log(`encodeURI ----- ${whatsAppText}`);
-
-	// https://wa.me/4407976612370?text=%E2%80%8E%20I%20want%20to%20share%20this%20music%20mix%20with%20you%20from%20---%20%20https%3A%2F%2Fwww.permy.co.uk%23123
-
 	return (
 		<div className="social-media-share-container">
 			<button className="share-button">
 				<ShareIcon />
 				<ul className="social-media-list">
 					<li>
-						<a href={`https://wa.me/?text=${whatsAppText}`} target="_blank">
+						<a href={`https://wa.me/?text=${whatsAppText}`} target="_blank" rel="noreferrer">
 							<WhatsAppIcon />
 						</a>
 					</li>
 					<li>
-						<a href={`https://www.facebook.com/sharer/sharer.php?u=${mixItemUrl}`} target="_blank" >
+						<a href={`https://www.facebook.com/sharer/sharer.php?u=${mixItemUrl}`} target="_blank" rel="noreferrer">
 							<FaceBookIcon />
 						</a>
 					</li>
 					<li>
-						<a href="instagram://user?username=permjitghataorre" target="_blank" >
+						<a href="instagram://user?username=permjitghataorre" target="_blank" rel="noreferrer">
 							<InstagramIcon />
 						</a>
 					</li>
 				</ul>
 			</button>
-		</div>
+		</div >
 	)
 }
 
